3cd5b4adb585e136874df2a1b281d346c077ff22
dolphin <yyan726@connect.hkust-gz.edu.cn>
2025-01-05 17:03:01 +0800

    update the model list

--- model_list.py
+++ model_list.py
@@ -58,48 +58,12 @@
 #         "/mnt/data/yuliangyan/microsoft/Phi-3-medium-4k-instruct" : 9,
 #         }
 
-MODEL_LIST = [
-  # Llama 3.1 Family.
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/Meta-Llama-3-8B-Instruct', 
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/Llama-3.1-SuperNova-Lite', 
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/Meta-Llama-3.1-8B-Instruct-GPTQ-Q_8', 
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/Llama-3.1-ARC-Potpourri-Induction-8B', 
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/Unsloth-Meta-Llama-3.1-8B-Instruct', 
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/Llama-3.1-8B-Instruct-FP8', 
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/llama_3.1_8b_prop_logic_ft', 
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/calme-2.3-legalkit-8b', 
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/Skywork', 
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/meta-llama_Llama-3.1-8B-Instruct-auto_gptq-int4-gs128-asym', 
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/SummLlama3.1-8B',
-  # Qwen2.5 Family.
-  '/mnt/data/hf_models/qwen-2.5-7b-instruct/Qwen2.5-7B-Instruct', 
-  '/mnt/data/hf_models/qwen-2.5-7b-instruct/Qwen-UMLS-7B-Instruct', 
-  '/mnt/data/hf_models/qwen-2.5-7b-instruct/Math-IIO-7B-Instruct', 
-  '/mnt/data/hf_models/qwen-2.5-7b-instruct/FinancialAdvice-Qwen2.5-7B', 
-  '/mnt/data/hf_models/qwen-2.5-7b-instruct/Qwen2.5-7B-Instruct-Uncensored', 
-  '/mnt/data/hf_models/qwen-2.5-7b-instruct/Qwen2.5-7B-Instruct-GPTQ-Int4', 
-  '/mnt/data/hf_models/qwen-2.5-7b-instruct/Qwen2.5-7B-Instruct-abliterated-v2', 
-  '/mnt/data/hf_models/qwen-2.5-7b-instruct/cybertron-v4-qw7B-UNAMGS', 
-  '/mnt/data/hf_models/qwen-2.5-7b-instruct/qwen2.5-7B-instruct-simpo', 
-  '/mnt/data/hf_models/qwen-2.5-7b-instruct/T.E-8.1',
-  # Mistral Family.
-  '/mnt/data/hf_models/mistral-7b-instruct/Mistral-7B-Instruct-v0.1', 
-  '/mnt/data/hf_models/mistral-7b-instruct/Mistral-base-instruct', 
-  '/mnt/data/hf_models/mistral-7b-instruct/mistral_docs_sum_p1_full', 
-  '/mnt/data/hf_models/mistral-7b-instruct/mistralai-Code-Instruct-Finetune-SG1-V5', 
-  # '/mnt/data/hf_models/mistral-7b-instruct/Mistral-7B-Instruct-v0.1-AWQ', 
-  '/mnt/data/hf_models/mistral-7b-instruct/Mistral-7B-Instruct-v0.1_asm_60e4dc58', 
-  '/mnt/data/hf_models/mistral-7b-instruct/original_glue_boolq', 
-  '/mnt/data/hf_models/mistral-7b-instruct/Mistral-7B-Instruct-v0.1-GPTQ', 
-  '/mnt/data/hf_models/mistral-7b-instruct/WeniGPT-Mistral-7B-instructBase', 
-  '/mnt/data/hf_models/mistral-7b-instruct/full_v2_astromistral', 
-  '/mnt/data/hf_models/mistral-7b-instruct/finetuned-mistral-7b',
-]
-
 # Model list for training the fingerprint extractor.
 MODEL_LIST_TRAIN = [
   # Llama 3.1 Family.
   '/mnt/data/hf_models/llama-3.1-8b-instruct/Meta-Llama-3.1-8B-Instruct', 
+  '/mnt/data/hf_models/llama-3.1-8b-instruct/Llama-3.1-8B-UltraMedical', # work
+  '/mnt/data/hf_models/llama-3.1-8b-instruct/Unsloth-Meta-Llama-3.1-8B-Instruct', # work
   '/mnt/data/hf_models/llama-3.1-8b-instruct/Llama-3.1-SuperNova-Lite', 
   '/mnt/data/hf_models/llama-3.1-8b-instruct/Llama-3.1-ARC-Potpourri-Induction-8B', 
   '/mnt/data/hf_models/llama-3.1-8b-instruct/Llama-3.1-8B-Instruct-FP8', 
@@ -115,10 +79,14 @@ MODEL_LIST_TRAIN = [
   '/mnt/data/hf_models/qwen-2.5-7b-instruct/FinancialAdvice-Qwen2.5-7B', 
   '/mnt/data/hf_models/qwen-2.5-7b-instruct/cybertron-v4-qw7B-UNAMGS', 
   '/mnt/data/hf_models/qwen-2.5-7b-instruct/qwen2.5-7B-instruct-simpo', 
+  '/mnt/data/hf_models/qwen-2.5-7b-instruct/Qwen2.5-7B-Instruct-Uncensored',
   '/mnt/data/hf_models/qwen-2.5-7b-instruct/T.E-8.1',
+  '/mnt/data/hf_models/qwen-2.5-7b-instruct/Qwen-Rui-SE',
   # Mistral Family.
   '/mnt/data/hf_models/mistral-7b-instruct/Mistral-7B-Instruct-v0.1', 
   '/mnt/data/hf_models/mistral-7b-instruct/astromistralv2', 
+  '/mnt/data/hf_models/mistral-7b-instruct/mistralai-Code-Instruct-Finetune-SG1-V5', # work
+  '/mnt/data/hf_models/mistral-7b-instruct/full_v2_astromistral', # work
   '/mnt/data/hf_models/mistral-7b-instruct/mistral_instruct_generation',
   '/mnt/data/hf_models/mistral-7b-instruct/radia-fine-tune-mistral-7b-lora',
   '/mnt/data/hf_models/mistral-7b-instruct/Mistral-7B-Instruct-v0.1_asm_60e4dc58', 
@@ -127,30 +95,9 @@ MODEL_LIST_TRAIN = [
   '/mnt/data/hf_models/mistral-7b-instruct/finetuned-mistral-7b',
 ]
 
-# Model list for evaluation the fingerprint extractor.
-MODEL_LIST_TEST = [
-  # Llama Family.
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/Meta-Llama-3.1-8B-Instruct', 
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/meta-llama_Llama-3.1-8B-Instruct-auto_gptq-int4-gs128-asym', # not work
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/Llama-3.1-8B-UltraMedical', # work
-  '/mnt/data/hf_models/llama-3.1-8b-instruct/Unsloth-Meta-Llama-3.1-8B-Instruct', # work
-  # '/mnt/data/hf_models/llama-3.1-8b-instruct/Meta-Llama-3.1-8B-Instruct-GPTQ-Q_8', # ?
-  # Qwen Family.
-  '/mnt/data/hf_models/qwen-2.5-7b-instruct/Qwen2.5-7B-Instruct', 
-  # '/mnt/data/hf_models/qwen-2.5-7b-instruct/Qwen2.5-7B-Instruct-AWQ', # not work
-  # '/mnt/data/hf_models/qwen-2.5-7b-instruct/Qwen2.5-7B-Instruct-abliterated-v2', # not work
-  # '/mnt/data/hf_models/qwen-2.5-7b-instruct/Qwen2.5-7B-Instruct-GPTQ-Int4', # not work
-  # '/mnt/data/hf_models/qwen-2.5-7b-instruct/Qwen2.5-7B-Instruct-GPTQ-Int8', # not work
-  # Mistral Family.
-  '/mnt/data/hf_models/mistral-7b-instruct/Mistral-7B-Instruct-v0.1', 
-  '/mnt/data/hf_models/mistral-7b-instruct/full_v2_astromistral', # work
-  '/mnt/data/hf_models/mistral-7b-instruct/mistralai-Code-Instruct-Finetune-SG1-V5', # work
-  '/mnt/data/hf_models/mistral-7b-instruct/Mistral-7B-Instruct-v0.1-GPTQ', # not work
-]
-
 MODEL_LIST_UNSEEN = [
   '/mnt/data/hf_models/llama-3.2-3b-instruct/Llama-3.2-3B-Instruct',
-  '/mnt/data/hf_models/llama-3.2-3b-instruct/Llama-Doctor-3.2-3B-Instruct', # not work
-  '/mnt/data/hf_models/llama-3.2-3b-instruct/Llama-Sentient-3.2-3B-Instruct', # not work
-  '/mnt/data/hf_models/llama-3.2-3b-instruct/Llama-3.2-3B-Instruct-4bit' # not work
+  '/mnt/data/hf_models/llama-3.2-3b-instruct/Llama-Doctor-3.2-3B-Instruct', # work
+  '/mnt/data/hf_models/llama-3.2-3b-instruct/Llama-Sentient-3.2-3B-Instruct', # work
+  '/mnt/data/hf_models/llama-3.2-3b-instruct/Llama-3.2-3B-Instruct-bnb-4bit',
 ]
