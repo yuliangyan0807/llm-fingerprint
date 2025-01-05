1098e49b67a5887bf9da6198a404619c3902c5bf
dolphin <yyan726@connect.hkust-gz.edu.cn>
2025-01-05 22:11:11 +0800

    ...

--- model_list.py
+++ model_list.py
@@ -20,44 +20,6 @@
 #               "/home/yuliangyan/Code/llm-fingerprinting/instruction_tuning_models/qwen25-ft",
 #               ]
 
-# MODEL_LABEL = ["llama3",
-#                "llama3-ft",
-#                "llama3-instruct",
-#                "llama3-instruct-ft",
-#                "llama3.1",
-#                "llama3.1-ft",
-#                "llama3.1-instruct",
-#                "llama3.1-instruct-ft",
-#                "mistral",
-#                "mistral-ft",
-#                "deepseek",
-#                "deepseek-ft",
-#                "deepseek-chat",
-#                "deepseek-chat-ft",
-#                "deepseek-math",
-#                "deepseek-math-ft",
-#                "qwen2.5",
-#                "qwen2.5-ft",
-#                ]
-
-# LABEL = {"/mnt/data/yuliangyan/meta-llama/Meta-Llama-3-8B/" : 0,
-#         "/home/yuliangyan/Code/llm-fingerprinting/stanford_alpaca/saved_models_llama3_8_test" : 0,
-#         "/mnt/data/yuliangyan/meta-llama/Meta-Llama-3-8B-Instruct/" : 1,
-#         "/mnt/data/yuliangyan/meta-llama/Meta-Llama-3.1-8B/" : 2,
-#         "/home/yuliangyan/Code/llm-fingerprinting/llama3_1_8_ft_tiny_test" : 2,
-#         "/home/yuliangyan/Code/llm-fingerprinting/llama3_1_8_ft_super_tiny_test" : 2,
-#         "/home/yuliangyan/Code/llm-fingerprinting/stanford_alpaca/saved_models_llama3_1_8_test" : 2,
-#         "/mnt/data/yuliangyan/meta-llama/Meta-Llama-3.1-8B-Instruct/" : 3,
-#         "/mnt/data/yuliangyan/mistralai/Mistral-7B-v0.1/" : 4,
-#         "/home/yuliangyan/Code/llm-fingerprinting/stanford_alpaca/saved_models_mistral_test" : 4,
-#         "/mnt/data/yuliangyan/deepseek-ai/deepseek-llm-7b-base/" : 5,
-#         "/home/yuliangyan/Code/llm-fingerprinting/stanford_alpaca/saved_models_deepseek_8_test" : 5,
-#         "/mnt/data/yuliangyan/deepseek-ai/deepseek-llm-7b-chat/" : 6,
-#         "/mnt/data/yuliangyan/deepseek-ai/deepseek-math-7b-instruct/" : 7,
-#         "/mnt/data/yuliangyan/Qwen/Qwen2.5-7B/" : 8,
-#         "/mnt/data/yuliangyan/microsoft/Phi-3-medium-4k-instruct" : 9,
-#         }
-
 # Model list for training the fingerprint extractor.
 MODEL_LIST_TRAIN = [
   # Llama 3.1 Family.
